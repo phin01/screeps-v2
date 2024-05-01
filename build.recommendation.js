eaf785e15241b9026f0b6577fc655a946d1929ff
Paulo Nascimento <phin01@gmail.com>
2024-05-01 12:19:02 -0300

    enhance logic for position of new extensions

--- build.recommendation.js
+++ build.recommendation.js
@@ -15,9 +15,9 @@ function buildRecommendation(SPAWN_NAME) {
     var recommendedPosition;
 
     // test path
-    // const destPosition = new RoomPosition(35, 15, spawnBase.room.name);
+    // const destPosition = new RoomPosition(32, 18, spawnBase.room.name);
     // const path = spawnBase.room.findPath(spawnBase.pos, destPosition);
-    // console.log(path.length);
+    // console.log(checkSpotAvailable(spawnBase, destPosition.x, destPosition.y));
 
 
     
@@ -27,15 +27,50 @@ function buildRecommendation(SPAWN_NAME) {
     // Roads
 
     // Build extensions according to controller level capacity
-    if(__f.filterByStructure(currentStructures, STRUCTURE_EXTENSION).length < getExtensionCapacity(room.controller.level)) {
-        
+    const currentExtensions = __f.filterByStructure(currentStructures, STRUCTURE_EXTENSION).concat( 
+                                __f.filterByStructure(currentConstructions, STRUCTURE_EXTENSION));
+
+    if(currentExtensions.length < getExtensionCapacity(room.controller.level)) {
         // first extension, build it around the spawn, ideally against a wall
-        if(__f.filterByStructure(currentStructures, STRUCTURE_EXTENSION).length == 0){
-            recommendedPosition = defineBuildPosition(spawnBase, 10, noAdjacent=false, nextToWall=true);
-            console.log(`${recommendedPosition.x} ${recommendedPosition.y}`);
+        if(currentExtensions.length == 0){
+            recommendedPosition = defineBuildPosition(spawnBase, 10, noOccupiedAdjacent=false, nextToWall=true, minSurroundingWalls=3);
+            if(recommendedPosition) {
+                buildStructure(spawnBase, recommendedPosition.x, recommendedPosition.y, STRUCTURE_EXTENSION);
+            }                
+            else{
+                console.log("Could not find a suitable location to build first EXTENSION");
+            }
         }
-        
-        // console.log('build ext');
+
+        // Additional extensions, prioritize building in locations:
+        // - Next to existing extensions
+        // - Next to walls
+        // - With as many adjacent free squares possible, for easier access
+        // Decrease restrictions if they don't match
+        else {
+            for (let i = 0; i < currentExtensions.length; i++) {
+                var ext = currentExtensions[i];
+                console.log(`Eval ${ext.pos.x} ${ext.pos.y}`);
+                recommendedPosition = defineBuildPosition(ext, 1, noOccupiedAdjacent=false, nextToWall=true, minSurroundingWalls=3);
+                if(recommendedPosition) {
+                    buildStructure(spawnBase, recommendedPosition.x, recommendedPosition.y, STRUCTURE_EXTENSION);
+                    break;
+                }
+                recommendedPosition = defineBuildPosition(ext, 1, noOccupiedAdjacent=false, nextToWall=true, minSurroundingWalls=2);
+                if(recommendedPosition) {
+                    buildStructure(spawnBase, recommendedPosition.x, recommendedPosition.y, STRUCTURE_EXTENSION);
+                    break;
+                }
+                recommendedPosition = defineBuildPosition(ext, 1, noOccupiedAdjacent=false, nextToWall=true, minSurroundingWalls=1);
+                if(recommendedPosition) {
+                    buildStructure(spawnBase, recommendedPosition.x, recommendedPosition.y, STRUCTURE_EXTENSION);
+                    break;
+                }
+                else{
+                    console.log(`Could not find a suitable location to build EXTENSION #${currentExtensions.length + 1}`);
+                }
+            }
+        }
     }
 
 
@@ -46,8 +81,7 @@ function buildRecommendation(SPAWN_NAME) {
         recommendedStructure = STRUCTURE_TOWER;
         recommendedPosition = defineBuildPosition(spawnBase, 5);
         console.log(`Building recommendation: TOWER at X:${recommendedPosition.x} Y:${recommendedPosition.y}`);
-        const result = spawnBase.room.createConstructionSite(recommendedPosition.x, recommendedPosition.y, STRUCTURE_TOWER);
-        console.log(result);
+        buildStructure(recommendedPosition.x, recommendedPosition.y, STRUCTURE_TOWER);
     }
 
 
@@ -55,9 +89,10 @@ function buildRecommendation(SPAWN_NAME) {
 
 
 
-function defineBuildPosition(referenceStructure, range, noOccupiedAdjacent=true, nextToWall=false, minFreeAdjacent=0){
+function defineBuildPosition(referenceStructure, range, noOccupiedAdjacent=true, nextToWall=false, minSurroundingWalls=0, flexSurroundingWalls=false){
     var longestDistance = 0;
     var mostDistant;
+    // console.log(`${flexSurroundingWalls} ${minSurroundingWalls}`);
 
     objects = referenceStructure.room.lookAtArea(referenceStructure.pos.y - range, 
                                                 referenceStructure.pos.x - range, 
@@ -65,26 +100,31 @@ function defineBuildPosition(referenceStructure, range, noOccupiedAdjacent=true,
                                                 referenceStructure.pos.x + range, true);
     for (let i = 0; i < objects.length; i++) {
         var currentDistance = getDistanceBetweenPoints(referenceStructure.pos.x, referenceStructure.pos.y, objects[i].x, objects[i].y);
-        if(currentDistance > longestDistance && 
-            checkSpotAvailable(referenceStructure, objects[i].x, objects[i].y) &&
+        if(currentDistance >= longestDistance && 
+            checkSpotAvailable(referenceStructure, objects[i].x, objects[i].y) && 
             checkIfNoAdjacentStructures(referenceStructure, objects[i].x, objects[i].y) == noOccupiedAdjacent &&
-            checkIfNextToWall(referenceStructure, objects[i].x, objects[i].y) == nextToWall
+            checkIfNextToWall(referenceStructure, objects[i].x, objects[i].y) == nextToWall &&
+            checkNumberAdjacentWalls(referenceStructure, objects[i].x, objects[i].y) >= minSurroundingWalls
         ) {
             longestDistance = currentDistance;
             mostDistant = i;
         };
     }
-    return objects[mostDistant];
+    if(mostDistant)
+        return objects[mostDistant];
+    if(flexSurroundingWalls && minSurroundingWalls > 1)
+        return defineBuildPosition(referenceStructure, range, noOccupiedAdjacent, nextToWall, minSurroundingWalls - 1, flexSurroundingWalls);
+    return false;
 }
 
 function getDistanceBetweenPoints(x1, y1, x2, y2){
-    return Math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    return Math.sqrt(Math.abs(x2 - x1)^2 + Math.abs(y2 - y1)^2);
 }
 
 function checkIfNoAdjacentStructures(referenceStructure, x, y){
     const adjacentSquares = referenceStructure.room.lookAtArea(y-1, x-1, y+1, x+1, true);
     for (let i = 0; i < adjacentSquares.length; i++) {
-        if(['source', 'structure', 'creep'].includes(adjacentSquares[i].type)) return false;
+        if(['source', 'structure', 'creep', 'constructionSite'].includes(adjacentSquares[i].type)) return false;
         if(['wall'].includes(adjacentSquares[i].terrain)) return false;
     }
     return true;
@@ -93,7 +133,7 @@ function checkIfNoAdjacentStructures(referenceStructure, x, y){
 function checkSpotAvailable(referenceStructure, spotX, spotY) {
     const spotDetails = referenceStructure.room.lookAt(spotX, spotY);
     for (let i = 0; i < spotDetails.length; i++){
-        if(['source', 'structure', 'creep'].includes(spotDetails[i].type)) return false;
+        if(['source', 'structure', 'creep', 'constructionSite'].includes(spotDetails[i].type)) return false;
         if(['wall'].includes(spotDetails[i].terrain)) return false;
     }
     return true;
@@ -107,6 +147,31 @@ function checkIfNextToWall(referenceStructure, x, y){
     return false;
 }
 
+function checkNumberAdjacentWalls(referenceStructure, x, y){
+    const adjacentSquares = referenceStructure.room.lookAtArea(y-1, x-1, y+1, x+1, true);
+    var adjacentWalls = 0;
+    for (let i = 0; i < adjacentSquares.length; i++) {
+        if(['wall'].includes(adjacentSquares[i].terrain))
+            adjacentWalls++;
+    }
+    return adjacentWalls;
+}
+
+function buildStructure(referenceStructure, x, y, structureType){
+    const result = referenceStructure.room.createConstructionSite(x, y, structureType);
+    if (result == 0){
+        console.log(`${structureType.toUpperCase()} queued for construction successfully`);
+    } else {
+        console.log(`ERROR: Could not build ${structureType.toUpperCase()} - Error Code ${result}`);
+    }
+}
+
+function listSpotDetails(referenceStructure, x, y){
+    const spotDetails = referenceStructure.room.lookAt(x, y);
+    for (let i = 0; i < spotDetails.length; i++){
+        console.log(`type: ${spotDetails[i].type} terrain: ${spotDetails[i].terrain}`);
+    }
+}
 
 function getExtensionCapacity(controllerLevel) {
     switch(controllerLevel) {
